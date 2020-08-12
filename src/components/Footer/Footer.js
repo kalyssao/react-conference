fc392bc2de4fa7b72eeb61171853ec0db9b0c2eb
kalyssao <klysssa@icloud.com>
2020-08-12 20:49:17 +0000

    footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <div className="FooterWrapper">
+            <h1>This is a footer</h1>
+        </div>
+    )
+}
