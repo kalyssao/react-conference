dd60b915afc3f1d32c5561b9c3357de1c869f86f
kalyssao <klysssa@icloud.com>
2020-08-16 20:43:52 +0000

    included react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+
+import history from './history'
 
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Carousel from './components/Carousel/Carousel'
 import Footer from './components/Footer/Footer'
+import Schedule from './components/Schedule/Schedule'
 
 function App() {
   return (
-    <div className="App">
-        <Navbar/>
-        <Hero/>
-        <Carousel/>
-        <Footer/>
-    </div>
+      <Router history={history} >
+          <div className="App">
+            <Navbar/>
+            <Switch>
+                <Route
+                  path="/schedule" component={Schedule}>
+                </Route>
+            </Switch>
+            <Hero/>
+            <Carousel/>
+            <Footer/>
+        </div>
+      </Router>
   );
 }
 
